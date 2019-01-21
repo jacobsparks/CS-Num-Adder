9e30c71f21585e0d3e41eb8612a35f91b9af977f
jacobsparks <38540611+jacobsparks@users.noreply.github.com>
2019-01-21 01:32:28 -0500

    Create cs-num-adder.cs

---
+++ cs-num-adder.cs
@@ -0,0 +1,24 @@
+using System;
+
+namespace NumAdder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sum = 0;
+
+            while (true)
+            { Console.WriteLine("Please enter a number to add to the sum, or enter 'calc' to get result.");
+                var input = Console.ReadLine();
+
+                if (input.ToLower() == "calc")
+                {
+                    break;
+                }
+                sum += Convert.ToInt32(input);
+            }
+            Console.WriteLine("The sum of all numbers is {0}", sum);
+        }
+    }
+}
